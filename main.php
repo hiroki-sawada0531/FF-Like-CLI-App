068f3909d77498cb06364b8f6e9b98e63d1863ff
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-04 23:54:04 +0900

    Whileで戦闘の繰り返し処理

--- main.php
+++ main.php
@@ -11,6 +11,8 @@ $goblin = new Enemy();
 $tiida->name = "ティーダ";
 $goblin->name = "ゴブリン";
 
+while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
+
 echo $tiida->name . "\n";
 echo $goblin->name . "\n";
 
@@ -22,3 +24,9 @@ $tiida->doAttack($goblin);
 echo "\n";
 $goblin->doAttack($tiida);
 echo "\n";
+
+}
+
+echo "★★★ 戦闘終了 ★★★\n\n";
+echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
