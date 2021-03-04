15396e5e918f51a5b75f4057cffa0f085d1eec42
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-04 23:09:30 +0900

    クラスのインスタンス化

--- main.php
+++ main.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "処理のはじまりはじまり〜！\n\n";
+// echo "処理のはじまりはじまり〜！\n\n";
+
+require_once('./classes/Human.php');
+require_once('./classes/Enemy.php');
+
+$tiida = new Human();
+$goblin = new Enemy();
+
+$tiida->name = "ティーダ";
+$goblin->name = "ゴブリン";
+
+echo $tiida->name . "\n";
+echo $goblin->name . "\n";
