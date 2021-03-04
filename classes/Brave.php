b25e3787d158000477cbb0a50e84e2981d81576e
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-05 00:58:26 +0900

    BraveクラスでdoAttackメソッドを上書き

--- classes/Brave.php
+++ classes/Brave.php
@@ -2,5 +2,16 @@
 
 class Brave extends Human
 {
-    
+    public function doAttack($enemy)
+    {
+        if (rand(1,3) === 1) {
+            echo "『" . $this->name . "』のスキルが発動した！\n";
+            echo "『ぜんりょくぎり』！！\n";
+            echo $enemy->name . "に" . $this->attackPoint * 1.5 . "のダメージ！\n";
+            $enemy->tookDamage($this->attackPoint * 1.5);
+        } else {
+            parent::doAttack($enemy);
+        }
+        return true;
+    }
 }
