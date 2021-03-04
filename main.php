e8c3b3eb58ef5839cfb76cf7324cdfc64d490ffe
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-04 23:46:02 +0900

    攻撃(互いのインスタンスの受け渡し)を実装

--- main.php
+++ main.php
@@ -17,3 +17,8 @@ echo $goblin->name . "\n";
 echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
 echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
 echo "\n";
+
+$tiida->doAttack($goblin);
+echo "\n";
+$goblin->doAttack($tiida);
+echo "\n";
