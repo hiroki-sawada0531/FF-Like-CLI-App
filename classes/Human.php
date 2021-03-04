200e6e7903d95574192e2070ef60840e78082659
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-04 20:40:12 +0900

    ダメージを受けるメソッドを定義

--- classes/Human.php
+++ classes/Human.php
@@ -13,4 +13,12 @@ class Human
         echo "【" . $enemy->name . "】に " . $this->attackPoint . " のダメージ！\n";
         $enemy->tookDamage($this->attackPoint);
     }
+
+    public function tookDamage($damage)
+    {
+        $this->hitPoint -= $damage;
+        if($this->hitPoint < 0) {
+            $this->hitPoint = 0;
+        }
+    }
 }
