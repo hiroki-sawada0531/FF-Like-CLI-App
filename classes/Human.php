5460df9823f85221783d03f8754995967c56da85
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-04 15:32:41 +0900

    「攻撃する」メソッドの宣言

--- classes/Human.php
+++ classes/Human.php
@@ -6,4 +6,11 @@ class Human
     public $name;
     public $hitPoint = 100;
     public $attackPoint = 20;
+
+    public function doAttack($enemy)
+    {
+        echo "『" .$this->name . "』の攻撃！\n";
+        echo "【" . $enemy->name . "】に " . $this->attackPoint . " のダメージ！\n";
+        $enemy->tookDamage($this->attackPoint);
+    }
 }
